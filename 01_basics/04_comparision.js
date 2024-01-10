398f5a67db50aa1fb6d45239531a81125d7b904d
Dipson Shrestha <117290336+Dipsonstha@users.noreply.github.com>
2024-01-10 18:07:10 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+//automatic typeconversion by js 
+// console.log("2">0);
+// console.log("2">true);
+// console.log("2">"one");
+
+console.log("2"==2);//only cheeks values
+console.log("2"===2);//strict check which checks values and datatype also
+console.log(2===2);
+console.log("two"==2);
