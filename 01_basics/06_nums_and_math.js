beb723d45f7c25a60d651bfb0c1c184981d1d0e9
Dipsonstha <dipson9861@gmail.com>
2024-01-13 00:26:53 +0545

    math in js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -7,13 +7,31 @@ const secondNumber = new Number(44); //It represents that its only number
 // console.log(typeof secondNumber);
 
 const total = 432;
-console.log(total.toFixed(3)); //return 3 digit after decimal
-console.log(total.toExponential(2)); //returns in decimal after 1st digit with exponential
-console.log(total.toPrecision(4));
-/* It gives the precision of how many digits we want to show before and after the decimal */
-console.log(total.toString().length); //converts into string and print the length
+// console.log(total.toFixed(3)); //return 3 digit after decimal
+// console.log(total.toExponential(2)); //returns in decimal after 1st digit with exponential
+// console.log(total.toPrecision(4));
+// /* It gives the precision of how many digits we want to show before and after the decimal */
+// console.log(total.toString().length); //converts into string and print the length
 
 const newnumber = 234444;
 //seprates numbers with coma (,)
-console.log(newnumber.toLocaleString()); //By default US standards
-console.log(newnumber.toLocaleString("en-In")); // Converting into indian standards
+// console.log(newnumber.toLocaleString()); //By default US standards
+// console.log(newnumber.toLocaleString("en-In")); // Converting into indian standards
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.abs(-4)); //converts -ve into +ve
+// console.log(Math.floor(4.6)); //4 cause it returns lower value no matter how big is decimal
+// console.log(Math.round(4.6)); //5 cause it after.5 it returns greater value
+//console.log(Math.ceil(4.1)); //5 cause after . it returns greater value
+
+///////////////////////////////
+console.log(Math.random()); //generates random from 0 to 1
+console.log(Math.random() * 10); // random from 0 to 10
+console.log(Math.random() * 10 + 1); // random from 1 to 10
+console.log(Math.floor(Math.random() * 10 + 1)); // random from 1 to 10 without decimals
+const min = 11;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // random from 11 to 20
+console.log(Math.floor(Math.random() * (max - min + 1)));
