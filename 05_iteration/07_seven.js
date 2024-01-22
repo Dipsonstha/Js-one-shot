8360204a869d1808c0dacda6394669835d06a60b
Dipsonstha <dipson9861@gmail.com>
2024-01-22 23:47:29 +0545

    .map practice

--- 05_iteration/07_seven.js
+++ 05_iteration/07_seven.js
@@ -12,3 +12,44 @@ const newNumbers = myNumbers
     return num >= 50;
   });
 console.log(newNumbers);
+
+const employee = [
+  {
+    id: "001",
+    age: 22,
+    salary: 20000,
+    name: "John Doe",
+  },
+  {
+    id: "002",
+    age: 18,
+    salary: 10000,
+    name: "Doe",
+  },
+  {
+    id: "003",
+    age: 30,
+    salary: 30000,
+    name: "Jesi",
+  },
+  {
+    id: "004",
+    age: 40,
+    salary: 20000,
+    name: "Eric",
+  },
+];
+const salary = employee.map((num) => num.salary);
+const totalSalary = employee.map((num) => {
+  if (num.salary >= 30000) {
+    console.log(`No salary bonus for you ${num.name}`);
+    return num.salary;
+  }
+  return num.salary * 2;
+});
+console.log(`The previous salary of employee is ${salary}`);
+console.log(`Salary with 50% of bonus of each employee is ${totalSalary}`);
+
+const age = [22, 33, 44, 11, 22, 55, 35, 43];
+const oldage = age.map((num) => num * 2).filter((num) => num > 60);
+console.log(`Old people ${oldage}`);
