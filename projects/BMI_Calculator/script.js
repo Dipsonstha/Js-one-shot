e3252afddc6dd524ea13217e2f542a90b4253f88
Dipsonstha <dipson9861@gmail.com>
2024-01-25 23:22:21 +0545

    bmi

---
+++ projects/BMI_Calculator/script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+//This  usecase gives empty
+//const height = parseInt(document.querySelector("#height").value);
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  //Get the value in integer from input field
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const result = document.querySelector("#result");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    result.innerHTML = `Please enter valid Height ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    result.innerHTML = `Please enter valid Weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    if (bmi <= 18.6) {
+      result.innerHTML = `<span>${bmi} UnderWeight</span>`;
+    } else if (bmi > 18.6 && bmi < 24.9) {
+      result.innerHTML = `<span>${bmi} Normal</span>`;
+    } else {
+      result.innerHTML = `<span>${bmi} Over Weight Fat Obbessed</span>`;
+    }
+  }
+});
