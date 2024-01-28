2ce871364cd4b970e8e04d55d277ace19476453f
Dipsonstha <dipson9861@gmail.com>
2024-01-28 21:59:52 +0545

    minor changes

--- projects/Clock/script.js
+++ projects/Clock/script.js
@@ -1,7 +1,10 @@
 const clock = document.getElementById("clock");
+const day = document.getElementById("date");
 
 setInterval(function () {
   let date = new Date();
+  day.innerHTML = date.toDateString();
+
   //   console.log(date.toLocaleTimeString());
   clock.innerHTML = date.toLocaleTimeString();
 }, 1000);
